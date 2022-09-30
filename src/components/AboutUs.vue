30b35930cdb4e6d4484010a58dff6de035a0e784
juliogver <jules.gravier@gmail.com>
2022-09-30 01:08:00 -0400

    modification about us/adding images

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,7 +1,7 @@
 <template>
 
 <!-- Remove the container if you want to extend the Footer to full width. -->
-<div class="container my-5 ">
+
   
   <footer class="text-white text-center text-lg-start bg-dark">
     <!-- Grid container -->
@@ -9,28 +9,30 @@
       <!--Grid row-->
       <div class="row mt-4">
         <!--Grid column-->
-        <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+        <div class="col-lg-4 col-md-12 mb-4 mb-md-0 ">
           <h5 class="text-uppercase mb-4">About company</h5>
 
           <p>
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-            voluptatum deleniti atque corrupti.
+            Our name is Freeprix, we are a company that sell occasion clothing on the internet.
           </p>
 
           <p>
-            Blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas
-            molestias.
+            We are a young company, created in 2022and our goal is to make you happy ! 
+          </p>
+
+          <p>
+            You can also check our social networks below : 
           </p>
 
           <div class="mt-4">
             <!-- Facebook -->
-            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-facebook-f"></i></a>
+            <a type="button" class="btn btn-floating btn-light btn-lg bg-dark"><img src="../assets/img/instagramlogo.png" alt="Bootstrap" width = "50" height = "50" class="img-rounded"><i class="fab fa-instagram"></i></a>
             <!-- Dribbble -->
-            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-dribbble"></i></a>
+            <a type="button" class="btn btn-floating btn-light btn-lg bg-dark"><img src="../assets/img/twitterlogo.png" alt="Bootstrap" width = "50" height = "50" class="img-rounded"><i class="fab fa-twitter"></i></a>
             <!-- Twitter -->
-            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-twitter"></i></a>
+            <a type="button" class="btn btn-floating btn-light btn-lg bg-dark"><img src="../assets/img/snapchatlogo.png" alt="Bootstrap" width = "50" height = "50" class="img-rounded"><i class="fab fa-snapchat"></i></a>
             <!-- Google + -->
-            <a type="button" class="btn btn-floating btn-light btn-lg"><i class="fab fa-google-plus-g"></i></a>
+            <a type="button" class="btn btn-floating btn-light btn-lg bg-dark"><img src="../assets/img/tiktoklogo.png" alt="Bootstrap" width = "50" height = "50" class="img-rounded"><i class="fab fa-tiktok"></i></a>
             <!-- Linkedin -->
           </div>
         </div>
@@ -38,22 +40,21 @@
 
         <!--Grid column-->
         <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-          <h5 class="text-uppercase mb-4 pb-1">Search something</h5>
+          <h5 class="text-uppercase mb-4 pb-1">Contact Us</h5>
+          
+          <img src="../assets/img/pinglogo.png" alt="Bootstrap" width = "50" height = "50" class="rounded mx-auto d-block"><p class = "mt-4 text-center">Stores in UE/UK/USA/CANADA</p>
 
-          <div class="form-outline form-white mb-4">
-            <input type="text" id="formControlLg" class="form-control form-control-lg" />
-            <label class="form-label" for="formControlLg">Search</label>
-          </div>
+    
 
           <ul class="fa-ul" style="margin-left: 1.65em;">
             <li class="mb-3">
-              <span class="fa-li"><i class="fas fa-home"></i></span><span class="ms-2">Warsaw, 00-967, Poland</span>
+              <span class="fa-li"><i class="fas fa-home"></i></span><span class="ms-2">Zoopal</span>
             </li>
             <li class="mb-3">
-              <span class="fa-li"><i class="fas fa-envelope"></i></span><span class="ms-2">contact@example.com</span>
+              <span class="fa-li"><i class="fas fa-envelope"></i></span><span class="ms-2">freeprixfr@gmail.com</span>
             </li>
             <li class="mb-3">
-              <span class="fa-li"><i class="fas fa-phone"></i></span><span class="ms-2">+ 48 234 567 88</span>
+              <span class="fa-li"><i class="fas fa-phone"></i></span><span class="ms-2">+ 33 91 91 91 91 91 </span>
             </li>
           </ul>
         </div>
@@ -61,24 +62,26 @@
 
         <!--Grid column-->
         <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-          <h5 class="text-uppercase mb-4">Opening hours</h5>
+          <h5 class="text-uppercase mb-4">Stores Opening hours</h5>
 
           <table class="table text-center text-white">
             <tbody class="fw-normal">
               <tr>
                 <td>Mon - Thu:</td>
-                <td>8am - 9pm</td>
+                <td>8am - 8pm</td>
               </tr>
               <tr>
                 <td>Fri - Sat:</td>
-                <td>8am - 1am</td>
+                <td>8am - 10 pm</td>
               </tr>
               <tr>
                 <td>Sunday:</td>
-                <td>9am - 10pm</td>
+                <td>9am - 8pm</td>
               </tr>
             </tbody>
           </table>
+          <img src="../assets/img/serpentlogo.png" alt="Bootstrap" width = "70" height = "70" class="rounded mx-auto d-block ">
+
         </div>
         <!--Grid column-->
       </div>
@@ -89,11 +92,11 @@
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
       © 2020 Copyright:
-      <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+      <a class="text-white" href="https://mdbootstrap.com/">Freeprix.com</a>
     </div>
     <!-- Copyright -->
   </footer>
 
-</div>
+
 <!-- End of .container -->
 </template>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from "./App.vue";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import "./assets/main.css";
+// import "./assets/main.css";
 
 createApp(App).mount("#app");
