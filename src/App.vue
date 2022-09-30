6a52458e92b6bcbefe4bb66e5556beb4edf297e5
juliogver <jules.gravier@gmail.com>
2022-09-30 02:10:34 -0400

    add current trends file/add carousel but not work

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import AboutUs from "./components/AboutUs.vue";
 import NavigationBar from "./components/NavigationBar.vue";
+import CurrentTrends from "./components/CurrentTrends.vue";
 </script>
 
 <template>
   <main>
     <NavigationBar />
+    <CurrentTrends />
     <AboutUs />
   </main>
 </template>
