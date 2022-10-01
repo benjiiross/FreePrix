2e2ec3db52fb966347cb86d56181d6014a3414f6
looadiiing <ulpenn@gmail.com>
2022-10-01 19:13:33 -0400

    adding bootstrap-icons

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 // import "./assets/main.css";
 
 createApp(App).mount("#app");
-
