302591aa6a955d460344b7f4c2d342d45f0a0451
juliogver <jules.gravier@gmail.com>
2022-10-12 14:37:32 -0400

    creating .env

--- api/app/config/db.config.js
+++ api/app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   HOST: "localhost",
   USER: "root",
-  PASSWORD: "7573B5F755",
+  PASSWORD: `${process.env.DB_PASSWORD}`,
   DB: "freeprix",
   dialect: "mysql",
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 };
--- api/server.js
+++ api/server.js
@@ -36,7 +36,7 @@ require("./app/routes/orderList.routes")(app);
 require("./app/routes/article.routes")(app);
 require("./app/routes/articleBought.routes")(app);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || `${process.env.BACKEND_PORT}`;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
